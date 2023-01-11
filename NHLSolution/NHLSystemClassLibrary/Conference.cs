441222adff242d3785dea526295828554e47dd41
John Joshua Ponce <jponce2@nait.ca>
2023-01-11 14:44:13 -0700

    commit

---
+++ NHLSolution/NHLSystemClassLibrary/Conference.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHLSystemClassLibrary
+{
+    internal enum Conference
+    {
+        Eastern,
+        Western,
+        Metropolitan,
+        Pacific,
+    }
+
+    static string Name()
+    {
+        
+    }
+}
